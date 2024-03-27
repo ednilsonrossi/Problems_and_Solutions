9739a7a6f15febe51da01ae3d0faa81e3afd37d9
Ednilson Rossi <ednilsonrossi@gmail.com>
2024-03-27 11:28:28 -0300

    problema 37 em C

---
+++ solutions/problem_0037/C/Main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+float read_total_account();
+int carlos_payment(float total);
+int andre_payment(float total);
+float felipe_payment(float total);
+
+int main(){
+    
+    float account;
+
+    account = read_total_account();
+
+    printf("Carlos.: R$ %d.00\n", carlos_payment(account));
+    printf("Andre..: R$ %d.00\n", andre_payment(account));
+    printf("Felipe.: R$ %.2f\n", felipe_payment(account));
+
+    return 0;
+}
+
+float read_total_account(){
+    float count;
+    printf("Digite total conta: \n");
+    scanf("%f", &count);
+    return count;
+}
+
+int carlos_payment(float total){
+    return total / 3;
+}
+
+int andre_payment(float total){
+    return carlos_payment(total);
+}
+
+float felipe_payment(float total){
+    return total - 2 * carlos_payment(total);
+}
